6d2c2cfc9166c5352843867aa522c0d14ab0c956
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-02-13 16:22:50 +0200

    Declare public properties

--- Sources/LazyLoadingPager/LazyLoadingPager.swift
+++ Sources/LazyLoadingPager/LazyLoadingPager.swift
@@ -1,22 +1,52 @@
 import SwiftUI
 
-struct LazyLoadingPagerView<Content: View, Data: Hashable>: View {
+public enum Direction {
+	case forward
+	case reverse
+
+	var toNavigationDirection: UIPageViewController.NavigationDirection {
+		switch self {
+			case .forward:
+					.forward
+			case .reverse:
+					.reverse
+		}
+	}
+}
+
+public struct LazyLoadingPagerView<Content: View, Data: Hashable>: View {
 	@Binding var data: Data
 	let initialContent: (Data) -> Content
 	let previous: (Data) -> Content?
 	let next: (Data) -> Content?
 	let previousData: (Data) -> Data?
 	let nextData: (Data) -> Data?
-	let scrollDirection: (Data, Data) -> UIPageViewController.NavigationDirection
+	let scrollDirection: (Data, Data) -> Direction
 
-	var body: some View {
+	public init(data: Binding<Data>,
+				initialContent: @escaping (Data) -> Content,
+				previous: @escaping (Data) -> Content?,
+				next: @escaping (Data) -> Content?,
+				previousData: @escaping (Data) -> Data?,
+				nextData: @escaping (Data) -> Data?,
+				scrollDirection: @escaping (Data, Data) -> Direction) {
+		_data = data
+		self.initialContent = initialContent
+		self.previous = previous
+		self.next = next
+		self.previousData = previousData
+		self.nextData = nextData
+		self.scrollDirection = scrollDirection
+	}
+
+	public var body: some View {
 		PagerView(data: $data,
 				  content: initialContent,
 				  previous: previous,
 				  next: next,
 				  previousData: previousData,
 				  nextData: nextData,
-				  scrollDirection: scrollDirection)
+				  scrollDirection: { scrollDirection($0, $1).toNavigationDirection })
 		.navigationBarHidden(true)
 	}
 }
